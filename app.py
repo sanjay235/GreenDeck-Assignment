6e1d86b62b677e681985dfc967a4e4eb40e83097
Sanjay <sanjayulsha@gmail.com>
2020-03-18 23:12:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from NetAPorter import NetAPorter
 from flask_cors import CORS
-import urllib.request
 import flask
 import os
 import gc
@@ -22,19 +21,8 @@ def home():
     print('Garbage Collector:', gc.collect())
     return result
 
-def init_files(dump_path = 'dumps/netaporter_gb.json'):
-    
-    if dump_path.split('/')[0] not in os.listdir():
-        os.mkdir(dump_path.split('/')[0])
-    
-    if not os.path.exists(dump_path):
-        urllib.request.urlretrieve(url, dump_path)
-
 # RUN FLASK APPLICATION
 if __name__ == '__main__':
-    
-    # GETTING DATASET this function will download the dataset
-    init_files('dumps/netaporter_gb.json') 
     
     # RUNNNING FLASK APP
     app.run(debug=True) 
