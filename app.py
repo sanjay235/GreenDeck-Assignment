e1b28daf3365b4e47d5355769b74e5cd9fc1e348
Sanjay <sanjayulsha@gmail.com>
2020-03-20 12:45:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
-from flask import Flask, request, jsonify, render_template
 from NetAPorter import NetAPorter
+
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import flask
 
-url = 'https://greendeck-datasets-2.s3.amazonaws.com/netaporter_gb_similar.json'
-
-# Importing the class
-netaporter = NetAPorter(path = url)
+URL = 'https://greendeck-datasets-2.s3.amazonaws.com/netaporter_gb_similar.json'
 
 # Create Flask application
 app = flask.Flask(__name__)
 CORS(app)
 
+# Importing the class
+netaporter = NetAPorter(path = URL)
+
 # Index page
-@app.route('/', methods=['POST'])
+@app.route('/')
 def home():
+    return 'This is GreenDeck NetAPorter API!!'
+
+@app.route('/getdata', methods=['POST'])
+def getData():
     return jsonify(netaporter.readQuery(request.get_json()))
 
 # RUN FLASK APPLICATION
 if __name__ == '__main__':
     
     # RUNNNING FLASK APP
-    app.run(debug=True) 
+    app.run(debug=True)
